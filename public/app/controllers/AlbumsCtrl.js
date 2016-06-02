c7ece6d7b84440ccbe52c05d26df2543389d9a12
Yassine Ragnar <yassinemorsaoui@gmail.com>
2016-06-02 14:40:24 +0100

    order by name albums

--- public/app/controllers/AlbumsCtrl.js
+++ public/app/controllers/AlbumsCtrl.js
@@ -2,6 +2,17 @@
 frontEnd.controller('AlbumsCtrl',function($scope,User,$location){
      $scope.userObj = User.get().userObj;
      $scope.albums=User.get().albums;
+
+     function compare(a,b) {
+		  if (a.name < b.name)
+		    return -1;
+		  else if (a.name > b.name)
+		    return 1;
+		  else 
+		    return 0;
+		}
+
+	 $scope.albums.sort(compare);
      $scope.albumPhotos=function(index){
         User.setIndex(index);
         $location.path("/photos");
